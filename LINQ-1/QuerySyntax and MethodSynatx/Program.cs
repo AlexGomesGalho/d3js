2b2b13e445658c90b418aed487bb588715e852af
adityAn9820 <adityan9820@gmail.com>
2020-04-24 19:06:08 +0530

    Linq

---
+++ LINQ-1/QuerySyntax and MethodSynatx/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace QuerySyntax_and_MethodSyntax
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var querysyantx = from obj in list
+                              where obj > 2
+                              select obj;
+            foreach (var i in querysyantx)
+            {
+                log.Debug(i);
+            }
+            log.Info("---------------------------");
+            var methodsyntax = list.Where(obj => obj > 2);
+            foreach (var i in methodsyntax)
+            {
+                log.Debug(i);
+            }
+            log.Info("---------------------------");
+
+            int mixedsyntax = (from obj in list
+                               select obj).Max();
+                log.Debug("MAx Value is: "+mixedsyntax);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LINQ-1/SortingOperator/SortingOperator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingOperators
+{
+    class OrderbyExample
+    {
+        static void Main(string[] args)
+        {
+            List<student> objStud = new List<student>()
+            {
+                new student() {Name= "Aditya", ID= "101" ,BSubject = new List<string> {"math","science"} },
+                new student() {Name= "Daksh", ID= "102" ,BSubject = new List<string> {"Geo","maths"} },
+                new student() {Name= "Raj", ID= "103" ,BSubject = new List<string> {"math","science"} },
+                new student() {Name= "Chetan", ID= "104" ,BSubject = new List<string> {"bio","science"} },
+                new student() {Name= "Yaseer", ID= "105" ,BSubject = new List<string> {"math","phy"} }
+            };
+
+            var studentname = objStud.OrderBy(x => x.Name);
+            foreach(var i in studentname)
+            {
+                Console.WriteLine(i.Name + " " +i.ID + " "+ i.BSubject.Count);
+                
+            }
+            Console.ReadLine();
+
+
+        }
+            class student
+        {
+            public string Name {get;set;}
+            public string ID { get; set; }
+            public List<string> BSubject { get; set; }
+        }
+    }
+}
