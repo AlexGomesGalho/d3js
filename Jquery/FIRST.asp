3ae5cd19153ee8f8526f5a3893169cc4bca261f6
adityAn9820 <adityan9820@gmail.com>
2020-04-24 19:07:38 +0530

    Jquery

---
+++ Jquery/FIRST.asp
@@ -0,0 +1,20 @@
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+  });
+
+</script>
+</head>
+<body>
+
+<p>If you click on me, I will disappear.</p>
+<p>Click me away!</p>
+<p>Click me too!</p>
+<button>Click me to hide the element</button>
+</body>
+</html>
---
+++ Jquery/Jquery_Basis/Jquery_Basis/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('li').css('color', 'blue').slideUp(1000).slideDown(1000), attr('title', 'Mytitle');
+});
+
+$(document).ready(() => {
+    $("#buttn1").click(() => {
+        $("id1").prepend("<Strong>Aditya</strong>");
+    });
+
+    $("#buttn2").click(() => {
+        $("ol").prepend("<li>Bangalore </li>")
+    })
+});
+
+$(document).ready(() => {
+    $("#btn3").click(function () {
+        $("h1,h2,p").addClass("blue");
+        $("#div9").addClass("important");
+    });
+});
+
+$(document).ready(function () {
+    $("#bttnnnnnnn").click(function () {
+        $("#divvvv").load("C:\Users\Bizruntime-50\source\repos\Aditya-N\Jquery\Jquery_Basis\Jquery_Basis\wwwroot\js\task.txt");
+    });
+});
