bd72cd3fc04f9edba112f5c64ab92c6eaf3ad1ba
adityAn9820 <adityan9820@gmail.com>
2020-04-24 19:10:13 +0530

    FileIO

---
+++ FileIO/ReadAllText/ReadAllText/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ReadAllTexts;
+
+namespace ReadAllText
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"C:\Users\Bizruntime-50\source\repos\Aditya-N\File IO\ReadAllText\Sample.txt";
+            var smallFilepath = @"C:\Users\Bizruntime-50\source\repos\Aditya-N\File IO\ReadAllText\ReadAllLineEx.txt";
+            ReadTextFile(path);
+
+            ReadAllLines rl = new ReadAllLines();
+            rl.ReadAllLineFile(smallFilepath);
+
+            var bytes = File.ReadAllBytes(path);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(bytes[i]);
+            }
+            Console.ReadLine();
+        }
+        public static void ReadTextFile(string path)
+        {
+            var text = File.ReadAllText(path);
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ FileIO/ReadAllText/ReadAllText/ReadAllLines.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadAllTexts
+{
+    class ReadAllLines
+    {
+        public void ReadAllLineFile(string path)
+        {
+            try
+            {
+                var line = File.ReadAllLines(path);
+                Console.WriteLine(line.Length);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
